243c0ca0383e4c769d21e8b90fd81ed070c43f1b
yangxing <yangxing820@163.com>
2018-05-29 17:41:56 +0800

    css

---
+++ mylibrary/src/main/java/applicate/test/com/mylibrary/MyUtils.java
@@ -0,0 +1,17 @@
+package applicate.test.com.mylibrary;
+
+/**
+ * 类描述：
+ * 创建人：yangxing
+ * 创建时间：2018/5/29 17:36
+ * 修改人：yangxing
+ * 修改时间：2018/5/29 17:36
+ * 修改备注：
+ */
+
+public class MyUtils {
+
+    public static void aa(){
+    }
+}
+
